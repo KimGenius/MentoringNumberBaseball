5e5bba80f1ddb5861d540031ee825d6c916f20d8
Kim0Jae <youngjae1047@naver.com>
2017-08-03 14:57:57 +0900

    Commit Message

--- src/NumberBaseball.kt
+++ src/NumberBaseball.kt
@@ -1,5 +1,36 @@
 import util.RandomUtil
+import java.util.*
+import kotlin.collections.ArrayList
+
+var level = 0
+var inputList = ArrayList<Int>()
+val scan = Scanner(System.`in`)
 
 fun main(args: Array<String>) {
-    RandomUtil.getNumbers(100)
+    val result = setNumbers()
+    startGame()
+}
+
+fun setNumbers(): ArrayList<Int> {
+    do {
+        print("1~10 정수형으로 난이도를 선택하세요 (높은 숫자일수록 난이도가 높아집니다) : ")
+        level = scan.nextInt()
+    } while (level > 10 || level < 0)
+    return RandomUtil.getNumbers(level)
+}
+
+fun startGame() {
+    do {
+        inputList.clear()
+        println("중복되지 않는 숫자(0~9) " + level + "개를 입력하세요")
+        (1..level).forEach {
+            inputList.add(scan.nextInt())
+        }
+        var bool = false
+        inputList.forEach { i ->
+            if (i < 0 || i > 9 && !bool) {
+                bool = true
+            }
+        }
+    } while (RandomUtil.checkOverlap(inputList) || bool)
 }
--- src/util/RandomUtil.kt
+++ src/util/RandomUtil.kt
@@ -2,16 +2,24 @@ package util
 
 import java.util.*
 import kotlin.collections.ArrayList
+import java.util.HashSet
+
 
 object RandomUtil {
-    private var rand = Random();
+    private var rand = Random()
     fun getNumbers(len: Int): ArrayList<Int> {
         val result = ArrayList<Int>()
-        for (i in 0..len - 1) {
-            result.add(rand.nextInt(10) + 0)
-            println(result[i])
-        }
-        println(result)
+        do {
+            result.clear()
+            for (i in 0..len - 1) {
+                val ran = rand.nextInt(10) + 0
+                result.add(ran)
+            }
+        } while (checkOverlap(result))
         return result
+    }
+
+    fun checkOverlap(arrayList: ArrayList<Int>): Boolean {
+        return arrayList.size != HashSet(arrayList).size
     }
 }
